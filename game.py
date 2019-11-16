e1e71422490f5172e5434c053e2ae45c4a06e9f4
heff <ksuder7@gmail.com>
2019-11-16 20:15:30 +0100

    add show board function

--- game.py
+++ game.py
@@ -3,18 +3,16 @@ board = ["-", "-", "-",
          "-", "-", "-"]
 
 
+def show_board():
+    print(board[0] + " | " + board[1] + " | " + board[2] + "\n" +
+          board[3] + " | " + board[4] + " | " + board[5] + "\n" +
+          board[6] + " | " + board[7] + " | " + board[8] + "\n")
 
 
-
-
-
-
-
-
-
+show_board()
 # game
 # players turns
 # check winner
- # check board
- # check if tie
+# check board
+# check if tie
 # switch player
